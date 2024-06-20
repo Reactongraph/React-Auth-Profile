d88e47bc92a2f5c27315d74f3c81b7600bd62236
Reactongraph <js.developer@ongraph.com>
2024-06-20 18:32:43 +0530

    not found page integrated and routes added

---
+++ src/container/NotFound/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { CCBox, CStack, CenterTextTypography } from "./Styled";
+import { Box, Button } from "@mui/material";
+
+const NotFound = () => {
+  return (
+    <CCBox>
+      <CStack spacing={3}>
+        <Box>
+          <img
+            alt="Under development"
+            src="/images/error-401.png"
+            width={100}
+            height={100}
+          />
+        </Box>
+        <CenterTextTypography variant="h3">
+          404: The page you are looking for isn&apos;t here
+        </CenterTextTypography>
+        <CenterTextTypography color="text.secondary" variant="body1">
+          You either tried some shady route or you came here by mistake.
+          Whichever it is, try using the navigation
+        </CenterTextTypography>
+        <Button
+          // component={RouterLink}
+          // href={paths.common.home}
+          // startIcon={<ArrowBackIcon />}
+          variant="contained"
+        >
+          Go back to home
+        </Button>
+      </CStack>
+    </CCBox>
+  );
+};
+
+export default NotFound;
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,15 +2,19 @@ import React from "react";
 import Login from "../container/Login";
 import { Route, Routes } from "react-router-dom";
 import Signup from "../container/Signup";
+import NotFound from "../container/NotFound";
+import Dashboard from "../container/Dashboard";
 
 const App = () => {
   return (
     <React.Suspense fallback="Loading...">
       <Routes>
         <Route>
+          <Route path="/" element={<Dashboard />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
         </Route>
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </React.Suspense>
   );
